ab5b256b8b3ed49e3737810879cb276f6a55936d
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2024-11-23 18:55:11 -0600

    Update TravelAssistant.java

--- HelpTravel/src/TravelAssistant.java
+++ HelpTravel/src/TravelAssistant.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -59,7 +60,6 @@ public class TravelAssistant extends Application {
         submitButton.setOnAction(e -> {
             String residence = residenceField.getText();
             String normalizedResidence = CountryNameNormalizer.normalizeCountryName(residence);
-            System.out.println("Residence: " + normalizedResidence);
             primaryStage.setScene(createDestinationScene(normalizedResidence));
         });
 
@@ -68,7 +68,7 @@ public class TravelAssistant extends Application {
         homeLayout.getChildren().addAll(welcomeLabel, residenceLabel, residenceField, submitButton);
         homeLayout.setAlignment(Pos.CENTER);
         homeLayout.setStyle("-fx-background-color: #f4f4f9; -fx-padding: 20;");
-        return new Scene(new StackPane(homeLayout), 900, 600);
+        return new Scene(new StackPane(homeLayout), 1000, 750);
     }
 
     // second page for destination user input
@@ -78,10 +78,25 @@ public class TravelAssistant extends Application {
         destinationLabel.setFont(new Font("Comic Sans MS", 16));
         destinationLabel.setTextFill(Color.rgb(100, 100, 100));
 
-        TextField destinationField = new TextField();
-        destinationField.setMaxWidth(300);
-        destinationField.setPromptText("Enter your destination");
-        destinationField.setStyle("-fx-background-radius: 15; -fx-padding: 10; -fx-border-color: #ccc; -fx-border-radius: 15;");
+        // interactive map
+        ImageView mapView = new ImageView("World_location_map_(equirectangular_180).png");
+        mapView.setFitWidth(900);
+        mapView.setPreserveRatio(true);
+
+        // action for clicking with mouse on the map
+        mapView.setOnMouseClicked(event -> {
+            // getting the longitude/latitude
+            double x = event.getX();
+            double y = event.getY();
+            double longitude = (x / mapView.getBoundsInParent().getWidth()) * 360 - 180;
+            double latitude = 90 - (y / mapView.getBoundsInParent().getHeight()) * 180;
+
+            ReverseGeocoding reverseGeocoding = new ReverseGeocoding();
+            String countryCode = reverseGeocoding.getCountryCode(latitude, longitude);
+            String destination = CountryCodeMapper.getCountryName(countryCode);
+
+            primaryStage.setScene(createInformationScene(normalizedResidence, destination));
+        });
 
         // back button to homepage
         Button backButton = new Button("<-Back");
@@ -91,39 +106,26 @@ public class TravelAssistant extends Application {
         backButton.setOnAction(e -> primaryStage.setScene(createHomeScene()));
         backButton.setEffect(new DropShadow(5, Color.GRAY));
 
-
-        Button destinationSubmitButton = new Button("Submit");
-        destinationSubmitButton.setStyle("-fx-background-color: #0056b3; -fx-text-fill: white; -fx-padding: 10 20; -fx-font-size: 16; -fx-background-radius: 12;");
-        destinationSubmitButton.setOnMouseEntered(e -> destinationSubmitButton.setStyle("-fx-background-color: #003f7f; -fx-text-fill: white; -fx-padding: 10 20; -fx-font-size: 16; -fx-background-radius: 12;"));
-        destinationSubmitButton.setOnMouseExited(e -> destinationSubmitButton.setStyle("-fx-background-color: #0056b3; -fx-text-fill: white; -fx-padding: 10 20; -fx-font-size: 16; -fx-background-radius: 12;"));
-        destinationSubmitButton.setEffect(new DropShadow(5, Color.GRAY));
-        destinationSubmitButton.setOnAction(e -> {
-            String destination = destinationField.getText();
-            String normalizedDestination = CountryNameNormalizer.normalizeCountryName(destination);
-            System.out.println("Destination: " + normalizedDestination);
-            primaryStage.setScene(createInformationScene(normalizedResidence, normalizedDestination));
-        });
-
-        // scene
+        // arrangement of components
         VBox destinationLayout = new VBox(15);
-        destinationLayout.getChildren().addAll(destinationLabel, destinationField, destinationSubmitButton, backButton);
+        destinationLayout.getChildren().addAll(destinationLabel, mapView, backButton);
         destinationLayout.setAlignment(Pos.CENTER);
         destinationLayout.setStyle("-fx-background-color: #f4f4f9; -fx-padding: 20;");
-        return new Scene(new StackPane(destinationLayout), 900, 600);
+        return new Scene(new StackPane(destinationLayout), 1000, 750);
     }
 
     // third page to display information about destination
-    private Scene createInformationScene(String normalizedResidence, String normalizedDestination) {
+    private Scene createInformationScene(String normalizedResidence, String destination) {
         // instances of classes and methods to get information
         TravelAdvisory Advisory = new TravelAdvisory();
-        String advisoryMessage = Advisory.getTravelAdvisory(normalizedDestination);
+        String advisoryMessage = Advisory.getTravelAdvisory(destination);
         String sourceCurrency = CountryCurrencyMapper.getCurrencyCode(normalizedResidence);
-        String targetCurrency = CountryCurrencyMapper.getCurrencyCode(normalizedDestination);
+        String targetCurrency = CountryCurrencyMapper.getCurrencyCode(destination);
         CurrencyConverter Converter = new CurrencyConverter();
         double exchangeRate = Converter.getExchangeRate(sourceCurrency, targetCurrency);
 
         // labels for advisory message
-        Label advisoryLabel = new Label("Travel Advisory for " + normalizedDestination + ":");
+        Label advisoryLabel = new Label("Travel Advisory for " + destination + ":");
         advisoryLabel.setFont(new Font("Comic Sans MS", 18));
         Label messageLabel = new Label(advisoryMessage);
         messageLabel.setFont(new Font("Comic Sans MS", 14));
@@ -135,10 +137,10 @@ public class TravelAssistant extends Application {
         moneyField.setPromptText("Enter amount");
         moneyField.setMaxWidth(200);
 
+        // converted amount
         Label convertedAmountLabel = new Label();
         convertedAmountLabel.setFont(new Font("Comic Sans MS", 14));
         convertedAmountLabel.setTextFill(Color.rgb(50, 50, 50));
-
         moneyField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue.isEmpty() || !newValue.matches("\\d+(\\.\\d{1,2})?")) {
                 convertedAmountLabel.setText("Please enter a valid amount.");
@@ -157,11 +159,11 @@ public class TravelAssistant extends Application {
         backButton.setOnAction(e -> primaryStage.setScene(createDestinationScene(normalizedResidence)));
         backButton.setEffect(new DropShadow(5, Color.GRAY));
 
-        // scene
+        // arrangement of components
         VBox informationLayout = new VBox(15);
         informationLayout.getChildren().addAll(advisoryLabel, messageLabel, moneyLabel, moneyField, convertedAmountLabel, backButton);
         informationLayout.setStyle("-fx-background-color: #f4f4f9; -fx-padding: 20;");
         informationLayout.setAlignment(Pos.CENTER);
-        return new Scene(informationLayout, 900, 600);
+        return new Scene(informationLayout, 1000, 750);
     }
 }
